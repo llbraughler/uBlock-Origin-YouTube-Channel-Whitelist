5112811d13686dfa89abc55abd81ebc8286bbe82
Lojjik Braughler <llbraughler@gmail.com>
2016-06-10 14:31:03 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# uBlock-Origin-YouTube-Channel-Whitelist
-Userscript for uBlock Origin by @gorhill
+# YouTube Channel Whitelist for uBlock Origin
+
+This userscript allows you to whitelist individual YouTube channels.
+
+Originally written by @gorhill and published here: https://greasyfork.org/en/scripts/13226-youtube-whitelist-channels-in-ublock-origin
+
+However, it is no longer being maintained. This repository aims to serve as a channel for active maintenance of this useful script.
+
+
+# Updates
+
+- Fixed "An error occurred during validation." occuring when viewing channel pages.
---
+++ uBlock Origin YouTube Channel Whitelist.user.js
@@ -0,0 +1,67 @@
+﻿// ==UserScript==
+// @name         YouTube - whitelist channels in uBlock Origin
+// @namespace    https://github.com/gorhill/uBlock
+// @version      1.5
+// @description  To whitelist YouTube channels in uBlock Origin
+// @author       Raymond Hill (gorhill)
+// @match        https://*.youtube.com/watch*
+// @grant        none
+// @license      http://creativecommons.org/licenses/by-sa/4.0/
+// @supportURL   https://github.com/gorhill/uBlock/issues
+// ==/UserScript==
+
+// First page load
+// 
+
+var exposeUserInURL = function() {
+    'use strict';
+ 
+    var link = document.querySelector('[id^="watch"][id$="-container"] [itemprop="channelId"][content]');
+    if ( link === null ) {
+       return;
+    }
+    var channelId = link.getAttribute('content') || '';
+    var newArg = channelId !== '' ? 'user=' + encodeURIComponent(channelId) : '';
+    var matches = location.search.match(/(?:[?&])(user=(?:[^&]+|$))/);
+    var oldArg = matches !== null ? matches[1] : '';
+    if ( newArg === oldArg ) {
+        return;
+    }
+    var href = location.href;
+    if ( oldArg === '' ) {
+        location.replace(href + (location.search === '' ? '?' : '&') + newArg);
+        return;
+    }
+    location.replace(href.replace(oldArg, newArg));
+};
+
+setTimeout(exposeUserInURL, 25);
+
+// DOM modifications
+
+var mutationHandlerTimer = null;
+
+var mutationHandlerAsync = function() {
+    'use strict';
+
+    mutationHandlerTimer = null;
+    exposeUserInURL();
+};
+
+var mutationHandler = function(mutations) {
+    'use strict';
+
+    if ( mutationHandlerTimer !== null ) {
+        return;
+    }
+
+    for ( var i = 0; i < mutations.length; i++ ) {
+        if ( mutations[i].addedNodes ) {
+            mutationHandlerTimer = setTimeout(mutationHandlerAsync, 25);
+            break;
+        }
+    }
+};
+
+var observer = new MutationObserver(mutationHandler);
+observer.observe(document.body, { childList: true, subtree: true });
